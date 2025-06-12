54a354dbe63ce1bdb17e9602ac2e7519233912f2
Eduardo-Barbosa <edu.silvab753@gmail.com>
2025-06-12 10:00:28 -0300

    Update calculadora_model.dart

--- PersistenciaDadosFlutter/calculadora_peso/lib/models/calculadora_model.dart
+++ PersistenciaDadosFlutter/calculadora_peso/lib/models/calculadora_model.dart
@@ -1,3 +1,21 @@
-class Caculadora {
-  
+class Peso {
+  final //double valor;
+  final String data; // formato: yyyy-MM-dd
+
+  Peso({required this.valor, required this.data});
+
+  Map<String, dynamic> toMap() {
+    return {
+      "valor": valor,
+      "data": data,
+    };
+  }
+
+  factory Peso.fromMap(Map<String, dynamic> map) {
+    return Peso(
+      valor: map["valor"],
+      data: map["data"],
+    );
+  }
+}
 }
